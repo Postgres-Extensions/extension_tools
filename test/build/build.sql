aadd55689427e344e8f25da79901231a007b9278
jnasbyupgrade <jnasby@upgrade.com>
2026-08-06 18:33:40 -0500

    test/build/build.sql: comment clarifying the dependency install line

--- test/build/build.sql
+++ test/build/build.sql
@@ -2,6 +2,9 @@
 BEGIN;
 \i test/pgxntool/psql.sql
 
+-- Install dependencies (via CREATE EXTENSION, not \i -- this is a real,
+-- already-published dependency being installed normally, not the extension
+-- whose raw install script this file exists to test line-by-line below).
 CREATE EXTENSION IF NOT EXISTS cat_tools;
 
 /*
